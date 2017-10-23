d1705be4f616fb865c1002c40d1476f0231ab62e
ragrog <ragrog216@outlook.jp>
2017-10-24 01:17:51 +0900

    不必要な出力を削除

--- main.go
+++ main.go
@@ -5,13 +5,11 @@ import (
 	"html"
 	"log"
 	"net/http"
-
-	"./config"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(config.Sql_user))
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
