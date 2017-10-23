dcaaa42ab1d2058296a6b3bebacf03188dff1cda
ragrog <ragrog216@gmail.com>
2017-10-24 06:46:00 +0900

    リクエストをメソッドごとに分けてみた

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	_"github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -12,5 +15,12 @@ func main() {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
+	router := httprouter.New()
+	router.GET("/koko:path", koko)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func koko(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Hello, get-koko %q", html.EscapeString(r.URL.Path) )
 }
