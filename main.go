b50a0ccad3084f28d6c90a24943aac6d8b8fb001
ragrog <ragrog216@gmail.com>
2017-10-24 09:01:16 +0900

    静的コンテンツ提供、kokoのデータと周囲のkokoを取得するAPIの雛形を作成

--- main.go
+++ main.go
@@ -2,25 +2,25 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
-	_"github.com/julienschmidt/httprouter"
 	"github.com/julienschmidt/httprouter"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	router := httprouter.New()
-	router.GET("/koko:path", koko)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
 func koko(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "Hello, get-koko %q", html.EscapeString(r.URL.Path) )
+	fmt.Fprintf(w, "koko")
 }
+func kokoaround(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "kokoaround")
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/koko", koko)
+	router.GET("/kokoaround", kokoaround)
+	router.ServeFiles("/image/*filepath", http.Dir("image/"))
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+
